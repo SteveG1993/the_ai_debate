b47feabb67cb6f5d34dd76a04117d883e465f102
Steve Gregoire <steveg93@gmail.com>
2025-07-02 00:36:15 -0400

    tailwindcsss changes

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"618yPD+VBlibsz5b1G767RuAUz1tXKHim2eGzaipnUI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
